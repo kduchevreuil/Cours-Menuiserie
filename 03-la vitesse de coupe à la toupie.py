1f615432d180adbd1b718aefc8238f4f6412734f
kduchevreuil <150837754+kduchevreuil@users.noreply.github.com>
2025-01-25 11:00:31 +0100

    Mise en place du dossier App

--- 03-la vitesse de coupe à la toupie.py
+++ 03-la vitesse de coupe à la toupie.py
@@ -1,13 +1,36 @@
 import math
 
+print('''
+      
+ ▌ ▐·▪  ▄▄▄▄▄▄▄▄ ..▄▄ · .▄▄ · ▄▄▄ .    ·▄▄▄▄  ▄▄▄ .     ▄▄·       ▄• ▄▌ ▄▄▄·▄▄▄ .
+▪█·█▌██ •██  ▀▄.▀·▐█ ▀. ▐█ ▀. ▀▄.▀·    ██▪ ██ ▀▄.▀·    ▐█ ▌▪▪     █▪██▌▐█ ▄█▀▄.▀·
+▐█▐█•▐█· ▐█.▪▐▀▀▪▄▄▀▀▀█▄▄▀▀▀█▄▐▀▀▪▄    ▐█· ▐█▌▐▀▀▪▄    ██ ▄▄ ▄█▀▄ █▌▐█▌ ██▀·▐▀▀▪▄
+ ███ ▐█▌ ▐█▌·▐█▄▄▌▐█▄▪▐█▐█▄▪▐█▐█▄▄▌    ██. ██ ▐█▄▄▌    ▐███▌▐█▌.▐▌▐█▄█▌▐█▪·•▐█▄▄▌
+. ▀  ▀▀▀ ▀▀▀  ▀▀▀  ▀▀▀▀  ▀▀▀▀  ▀▀▀     ▀▀▀▀▀•  ▀▀▀     ·▀▀▀  ▀█▄▀▪ ▀▀▀ .▀    ▀▀▀ 
+ ▄▄▄·     ▄▄▌   ▄▄▄·     ▄▄▄▄▄      ▄• ▄▌ ▄▄▄·▪  ▄▄▄ .                           
+▐█ ▀█     ██•  ▐█ ▀█     •██  ▪     █▪██▌▐█ ▄███ ▀▄.▀·                           
+▄█▀▀█     ██▪  ▄█▀▀█      ▐█.▪ ▄█▀▄ █▌▐█▌ ██▀·▐█·▐▀▀▪▄                           
+▐█ ▪▐▌    ▐█▌▐▌▐█ ▪▐▌     ▐█▌·▐█▌.▐▌▐█▄█▌▐█▪·•▐█▌▐█▄▄▌                           
+ ▀  ▀     .▀▀▀  ▀  ▀      ▀▀▀  ▀█▄▀▪ ▀▀▀ .▀   ▀▀▀ ▀▀▀                                                              
+                                                                                                                                                                        ░                ░                                          
+     ''')
+
+
 def calculer_vitesse_coupe():
     # Demander à l'utilisateur de saisir le diamètre de l'outil
     diametre_input = input("Entrez le diamètre de l'outil en mètres : ")
     # Convertir la saisie en un nombre flottant (float)
     diametre_outil = float(diametre_input)
 
+    # Convertir le diamètre de l'outil en millimètres et centimètres
+    diametre_outil_mm = diametre_outil * 1000
+    diametre_outil_cm = diametre_outil * 100
+    print(f"Le diamètre de l'outil est de {diametre_outil_mm} mm.")
+    print(f"Le diamètre de l'outil est de {diametre_outil_cm} cm.")
+
     # Demander à l'utilisateur de saisir la vitesse de rotation de la toupie en tr/min
-    vitesse_input = input("Entrez la vitesse de rotation de la toupie en tr/min : ")
+    vitesse_input = input(
+        "Entrez la vitesse de rotation de la toupie en tr/min : ")
     # Convertir la saisie en un nombre flottant (float)
     vitesse_rotation = float(vitesse_input)
 
@@ -16,6 +39,11 @@ def calculer_vitesse_coupe():
 
     # Afficher le résultat avec une précision de deux décimales en mètres par minute
     print(f"La vitesse de coupe est de {vitesse_coupe} m/min.")
+
+    # convertir la vitesse de coupe en km/h
+    vitesse_coupe_kmh = vitesse_coupe * 60 / 1000
+    print(f"La vitesse de coupe est  de {vitesse_coupe_kmh} km/h.")
+
 
 # Appeler la fonction pour exécuter le code
 calculer_vitesse_coupe()
